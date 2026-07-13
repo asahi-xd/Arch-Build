7cc77111382fea5904f24157534854e0a07280a5
Asahi <217256082+asahi-xd@users.noreply.github.com>
2026-07-13 11:21:34 +0530

    some work
    done

--- test.sh
+++ test.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
+if [ $(id --user) -eq 0 ]; then
+    echo "Do not run this script as root."
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo "$# packages provided as argument."
+    exit 1
+fi
+
 WORKFLOW="aur-build.yml"
 REPO="asahi-xd/AUR-Build"
 LOCAL_REPO_NAME="gh-aur-builds"
-LOCAL_REPO_DIR="/mnt/Data_Drive/.local/repo"
+LOCAL_REPO_DIR="/mnt/Data_Drive/.local/pacman/repo"
+DOWNLOAD_TMP_DIR="/mnt/Data_Drive/.local/pacman/tmp"
 
 echo "Verifying that the package names provided are valid...\n"
 
 packages=("$@")
-errcount=0
 
-for i in ${packages[@]}; do
-    # check if yay -Si output contains "No AUR package found" text.
-    # if it does then errcount+=1
-    # Also increment i to a list
+for i in ${packages[@]}: do
+    if ! $( yay -Si $i ); then
+        invalid_pkgs+=($i)
+    fi
+done
 
-if errcount > 0:
-    # exit the script
-    echo "Invalid package names provided:"
-    echo "That list"
-    exit
+if [ ${#invalid_pkgs[@]} -ne 0 ]; then
+    echo "Invalid package name(s) provided:"
+    echo ${invalid_pkgs[@]}
+    exit 1
+fi
 
+
+
+
+
+# If the packages were valid, then add a logic block here that asks the user if they want to
+# Read the respective PKGBUILDs, by opening them One by one with less
+# Then have the user confirm that they wish to continue, and then continue or exit the script.
 
 echo "Running the workflow...\n"
-echo "Building packages: $@"
+echo "Building package(s): $@"
 
 gh workflow run \
     "$WORKFLOW" -R "$REPO" \
@@ -31,14 +49,16 @@ gh workflow run \
     && sleep 5 \
     && RUN_ID=$(gh run list --workflow=$WORKFLOW -R $REPO --limit 1 --json databaseId --jq '.[0].databaseId') \
     && gh run -R "$REPO" watch $RUN_ID \
-    && gh run -R "$REPO" download $RUN_ID \
+    && gh run -R "$REPO" download $RUN_ID --dir "$DOWNLOAD_TMP_DIR" \
     && ARTIFACT_NAME=$(gh api repos/$REPO/actions/runs/$RUN_ID/artifacts --jq '.artifacts[0].name') \
     && echo $ARTIFACT_NAME
     # This should then move the package files to the local repo directory,
     # and return some kind of confirmation that its done
     # ... WIP
 
-# Add a logic block to have it execute iff the workflow ran properly without errors.
+echo "Adding package(s) to local repository..."
+
+# Add a logic block to have the below part execute iff the workflow ran properly without errors.
 repo-add "$LOCAL_REPO_DIR"/"$LOCAL_REPO_NAME"/"$LOCAL_REPO_NAME".db.tar.zst ./*.pkg.tar.zst
 
 
